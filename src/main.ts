4577b3a13c0b2d9d53cc5311cff2d55feb14f8b4
AnatolyFiv <64248582+AnatolyFiv@users.noreply.github.com>
2025-11-30 20:18:15 +0300

    feat: update README and api.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+import * as yaml from 'js-yaml';
 import 'dotenv/config';
 
 async function bootstrap() {
@@ -15,6 +19,18 @@ async function bootstrap() {
       },
     }),
   );
+
+  try {
+    const apiYamlPath = path.join(process.cwd(), 'doc', 'api.yaml');
+    if (fs.existsSync(apiYamlPath)) {
+      const apiYamlContent = fs.readFileSync(apiYamlPath, 'utf8');
+      const document = yaml.load(apiYamlContent) as OpenAPIObject;
+      if (document) {
+        SwaggerModule.setup('doc', app, document);
+      }
+    }
+  } catch (error) {}
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
