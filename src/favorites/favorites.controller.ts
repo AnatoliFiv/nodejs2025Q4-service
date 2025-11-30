b35b031d57ee7f1748b385bfbfc4b3a323c0f979
AnatolyFiv <64248582+AnatolyFiv@users.noreply.github.com>
2025-11-30 17:07:58 +0300

    feat: add controllers

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Param,
+  HttpCode,
+  HttpStatus,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { FavoritesService } from './favorites.service';
+import { Artist } from '../artists/entities/artist.entity';
+import { Album } from '../albums/entities/album.entity';
+import { Track } from '../tracks/entities/track.entity';
 
 @Controller('favs')
-export class FavoritesController {}
+export class FavoritesController {
+  constructor(private readonly favoritesService: FavoritesService) {}
+
+  @Get()
+  async findAll(): Promise<{
+    artists: Artist[];
+    albums: Album[];
+    tracks: Track[];
+  }> {
+    return this.favoritesService.findAll();
+  }
+
+  @Post('track/:id')
+  @HttpCode(HttpStatus.CREATED)
+  async addTrack(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.favoritesService.addTrack(id);
+  }
+
+  @Delete('track/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async removeTrack(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.favoritesService.removeTrack(id);
+  }
+
+  @Post('album/:id')
+  @HttpCode(HttpStatus.CREATED)
+  async addAlbum(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.favoritesService.addAlbum(id);
+  }
+
+  @Delete('album/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async removeAlbum(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.favoritesService.removeAlbum(id);
+  }
+
+  @Post('artist/:id')
+  @HttpCode(HttpStatus.CREATED)
+  async addArtist(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.favoritesService.addArtist(id);
+  }
+
+  @Delete('artist/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async removeArtist(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.favoritesService.removeArtist(id);
+  }
+}
--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+  HttpStatus,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { TracksService } from './tracks.service';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+import { Track } from './entities/track.entity';
 
 @Controller('track')
-export class TracksController {}
+export class TracksController {
+  constructor(private readonly tracksService: TracksService) {}
+
+  @Get()
+  async findAll(): Promise<Track[]> {
+    return this.tracksService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id', ParseUUIDPipe) id: string): Promise<Track> {
+    return this.tracksService.findById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createTrackDto: CreateTrackDto): Promise<Track> {
+    return this.tracksService.create(createTrackDto);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateTrackDto: UpdateTrackDto,
+  ): Promise<Track> {
+    return this.tracksService.update(id, updateTrackDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+    await this.tracksService.delete(id);
+  }
+}
